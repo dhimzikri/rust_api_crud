ce54cdac0683ebe8ade3ea1bcb1beda4ac7f42e6
Dimas Zikri <dschok182@gmail.com>
2024-12-27 18:06:13 +0700

    []

--- src/gridcase.rs
+++ src/gridcase.rs
@@ -1,7 +1,7 @@
 use sqlx::{query, query_as ,Mssql, Pool};
 use sqlx::Row;
 use sqlx::mssql::MssqlPool;
-use sqlx::error::DatabaseError;
+use sqlx::{Error as SqlxError, DatabaseError};
 use serde_json::Value;
 use std::collections::HashMap;
 use chrono::NaiveDateTime;
