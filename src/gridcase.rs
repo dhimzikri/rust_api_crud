631331b8eb5e576bbea214952a05e879dd304059
Dimas Zikri <dschok182@gmail.com>
2024-12-31 09:31:50 +0700

    trydtmupd

--- src/gridcase.rs
+++ src/gridcase.rs
@@ -202,6 +202,14 @@ pub async fn getCase(
     let start = start.unwrap_or(0);
     let limit = limit.unwrap_or(10);
     let count_last = start + limit;
+    let dtmupd: String = match row.try_get::<Option<String>, _>("a.foragingdays")? {
+        Some(dtm) => {
+            NaiveDateTime::parse_from_str(&dtm, "%Y-%m-%d %H:%M:%S")
+                .map(|dt| dt.format("%Y-%m-%d %H:%M:%S").to_string()) // Ensure the format matches 2020-12-06 18:55:30
+                .unwrap_or_else(|_| "".to_string()) // Handle invalid datetime formats
+        }
+        None => "".to_string(), // Handle NULL case
+    };
 
     // Base query condition
     let mut src = format!("0=0 AND a.statusid <> 1 AND a.usrupd = '{}'", user_name);
@@ -223,7 +231,7 @@ pub async fn getCase(
                     ORDER BY
                     RIGHT(a.ticketno, 3) DESC
                 ) AS 'RowNumber',
-                a.flagcompany, a.agreementno, a.foragingdays
+                a.flagcompany, a.agreementno, CONVERT(VARCHAR, a.foragingdays, 120)
                 FROM
                 "Case" a
                 INNER JOIN tbltype b ON a.TypeID = b.TypeID
